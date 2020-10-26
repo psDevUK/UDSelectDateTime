b6e59b2d68f75ee94080ec1b0ed7b735135ae329
Adam <adambacon1@hotmail.com>
2020-10-26 21:19:11 +0000

    Add files via upload

---
+++ UniversalDashboard.MaskInput/MaskInput.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MaskedInput from 'react-text-mask'
+class MaskInput extends React.Component {
+
+  // state is for keeping control state before or after changes.
+  state = {
+    // the things you want to put in state.
+    // text: this.props.text //un comment the line to use state insted props
+  }
+
+
+  render() {
+
+    return (
+      <MaskedInput
+        mask={[/[1-2]/, /[0-9]/, /[0-9]/, '.', /[1-2]/, /[0-9]/, /[0-9]/, '.', /[1-2]/, /[0-9]/, /[0-9]/, '.', /[1-2]/, /[0-9]/, /[0-9]/]}
+        className={this.props.className}
+        placeholder={this.props.placeholder}
+        guide={this.props.guide}
+      // onChange={() => { }}
+      />
+    );
+
+  }
+}
+
+export default MaskInput
